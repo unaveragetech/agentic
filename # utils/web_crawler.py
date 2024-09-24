f16f73fe222a728570da6204f185dd5a8b182323
jesse winghart <llewknight88@gmail.com>
2024-09-23 22:29:42 -0400

    Update web_crawler.py

--- # utils/web_crawler.py
+++ # utils/web_crawler.py
@@ -1,24 +1,28 @@
 # utils/web_crawler.py
 
 import requests
+from requests.exceptions import RequestException
 from bs4 import BeautifulSoup
 
 class WebCrawler:
-    def __init__(self):
+    def __init__(self, user_agent, timeout):
         self.visited_urls = set()
+        self.user_agent = user_agent
+        self.timeout = timeout
 
     def crawl(self, url):
-        # Crawl a given URL and return the HTML content
         if url in self.visited_urls:
             print(f"Already visited {url}. Skipping...")
             return None
-        
+
+        headers = {'User-Agent': self.user_agent}
         print(f"Crawling URL: {url}")
-        response = requests.get(url)
-        
-        if response.status_code == 200:
+        try:
+            response = requests.get(url, headers=headers, timeout=self.timeout)
+            response.raise_for_status()
             self.visited_urls.add(url)
             return response.text
-        else:
-            print(f"Failed to crawl {url}. Status code: {response.status_code}")
+        except RequestException as e:
+            print(f"Error crawling {url}: {e}")
             return None
+
